9cdfe566b754b5bf158b2b74814f8037953d7cf9
Arinze <calistusarinzechinweuba@gmail.com>
2023-10-25 08:46:38 +0100

    Add homepage-js

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div className='homepage'>
+      <div className="homepage__nav">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
