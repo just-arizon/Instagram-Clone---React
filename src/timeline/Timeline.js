5bbdcd8a2b0fe045e78c87c164f1e6999465ae4d
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-11-06 17:13:12 +0100

    Edit post.css

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Timeline.css'
 import Suggestion from './Suggestion';
 import Post from './posts/Post';
 
 function Timeline() {
+  const [posts, setPosts] = useState([
+    {
+      user: "Arizon",
+      postImage: "https://www.pexels.com/photo/silhouette-of-man-standing-near-body-of-water-1136571/",
+      Likes: 12,
+      timestamps: "4hr ago",
+    }
+    ,
+    {
+      user: "Magret Jennifer",
+      postImage: "https://images.pexels.com/photos/1101726/pexels-photo-1101726.jpeg?auto=compress&cs=tinysrgb&w=600",
+      Likes: 56,
+      timestamps: "1d ago",
+    }
+    ,
+    {
+      user: "James Burden",
+      postImage: "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=600",
+      Likes: 102,
+      timestamps: "2h ago",
+    }
+    ,
+    {
+      user: "Christian",
+      postImage: "https://images.pexels.com/photos/765217/pexels-photo-765217.jpeg?auto=compress&cs=tinysrgb&w=600",
+      Likes: 122,
+      timestamps:  "2d",
+    }
+    
+  ]);
   return (
     <div className='timeline'>
          <div className='timeline__left'>
            <div className='timeline__post'>
+           {posts.map((post) => (
             <Post />
-            <Post />
-            <Post />
+          ))}
            </div>
         </div>
         <div className='timeline__right'>
--- src/timeline/posts/Post.js
+++ src/timeline/posts/Post.js
@@ -7,13 +7,25 @@ function post() {
     <div className='post'>
       <div className='post__header'>
         <div className='post__headerAuthor'>
-        <Icon icon="carbon:user-avatar-filled" /> Arizon . <span>12h</span>
+        <Icon icon="carbon:user-avatar-filled" className='postIcon'/> Arizon • <span>12h</span>
         </div>
+        <Icon icon="pepicons-pop:dots-x" />
       </div>
       <div className='post__image'>
-
+        <img src='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=600' alt=''/>
       </div>
       <div className='post__footer'>
+         <div className='post__footerIcons'>
+          <div className='post__iconsMain'>
+          <Icon icon="carbon:favorite" className='postIcon'/>
+          <Icon icon="ic:twotone-chat-bubble-outline" className='postIcon'/>
+          <Icon icon="cil:send" className='postIcon'/>  
+          </div>
+          <div className='post__iconsSave'>
+          <Icon icon="material-symbols:bookmark-outline" className='postIcon'/>
+          </div>
+         </div>
+         Liked by 21 people
 
       </div>
     </div>
