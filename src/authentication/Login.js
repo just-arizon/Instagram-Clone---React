3f6221840730e506fe2d41560373065d86db574b
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-12-30 10:10:11 +0100

    Adjust Authentication responsiveness

--- src/authentication/Login.js
+++ src/authentication/Login.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Login.css"
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className='login'>
       <img
         src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
         alt=""
       />
-      <input type='email' placeholder='Email'/>
-      <input type='password' placeholder='Password'/>
+      <input onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Email' value={email}/>
+      <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Password' value={password}/>
       <button>Log in</button>
     </div>
   );
--- src/authentication/Signup.js
+++ src/authentication/Signup.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./Signup.css"
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
   return (
     <div className='signup'>
        <img
         src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
         alt=""
       />
-      <input type='email' placeholder='Email'/>
-      <input type='email' placeholder='Username'/>
-      <input type='password' placeholder='Password' />
+      <input onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Email'value={email}/>
+      <input onChange={(e) => setUsername(e.target.value)} type='email' placeholder='Username'value={username}/>
+      <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Password' value={password}/>
       <button>Sign up</button>
     </div>
   );
