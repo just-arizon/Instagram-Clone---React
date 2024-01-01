8d2ced055b18a17401532bf95e7e03499e8b09b5
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-01-01 01:42:24 +0100

    Navigation commit

--- src/navigation/Sidenav.js
+++ src/navigation/Sidenav.js
@@ -14,9 +14,11 @@ function Sidenav() {
       </div>
     <div className='sidenav__buttons'>
       <div className="btn-container">
-        <button className='sidenav__button'>
+        <button className='sidenav__button home_btn'>
           {/* Icons */}
-          <svg aria-label="Home" class="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z"></path></svg>
+          <div className="home">
+            <svg aria-label="Home" class="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z"></path></svg>
+          </div>
           <span>Home</span>
         </button>
         <button className='sidenav__button search_btn'>
